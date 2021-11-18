be208334ae3de57d51b5a76a92f22aeab9030488
Shashwat <shashwatpattnayak2001@gmail.com>
2021-11-18 11:50:00 +0530

    added module 4 solution

---
+++ module4_solution/script.js
@@ -0,0 +1,12 @@
+(function () {
+    var names = ["Yoni", "Jackson", "Jimmy", "Jack", "Priyabrata", "Fren", "Lumia", "Pagal", "Linda", "Joker"];
+    
+    for(var i = 0; i < names.length; i++){
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter == 'j') {
+        byeSpeaker.speak(names[i]);
+      } else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+})();
